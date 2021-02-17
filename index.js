74188953c8732181e128c087446a20380083bb1c
Tarek Behairy <tbtarek@gmail.com>
2021-02-16 21:10:57 -0500

    added inquirere prompts for initial promt and manager creation prompt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const validate = require('./src/validate');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -7,3 +8,60 @@ const managerHTML = require('./html/managerTemplate');
 const engineerHTML = require('./html/engineerTemplate');
 const internHTML = require('./html/internTemplate');
 const renderHTML = require('./html/finalTemplate');
+
+const MembersArr = [];
+let teamName = "";
+
+const startPrompt = () => {
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Hello. what is your team\'s name?',
+      name: 'teamname',
+      validate: validate.notEmpty
+    }
+  ])
+    .then((data) => {
+      teamName = data.teamname;
+      managerAddition()
+    })
+}
+
+const managerAddition = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the manager\'s name?',
+      name: 'name',
+      validate: validate.notEmpty
+    },
+    {
+      type: 'input',
+      message: 'What is the manager\'s employee ID?',
+      name: 'id',
+      validate: validate.numbersOnly
+    },
+    {
+      type: 'input',
+      message: 'What is the manager\'s email address?',
+      name: 'email',
+      validate: validate.email
+    },
+    {
+      type: 'input',
+      message: 'What is the manager\'s office number?',
+      name: 'officeNumber',
+      validate: validate.numbersOnly
+    },
+
+  ]).then(function ({ name, id, email, officeNumber }) {
+    let newManager;
+    newManager = new Manager(name, id, email, officeNumber);
+    teamMembersArr.push(newManager);
+    addEmployee();
+
+
+  });
+
+}
